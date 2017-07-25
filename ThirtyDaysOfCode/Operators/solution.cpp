5a66f60a99d7b8e2064a3a5dc4804bf97fd38b53
Saydul Bashar <tamimnoone@gmail.com>
2017-07-25 11:30:09 +0000

    Completed the solution for Operators.

--- ThirtyDaysOfCode/Operators/solution.cpp
+++ ThirtyDaysOfCode/Operators/solution.cpp
@@ -7,8 +7,19 @@ double Solution::amountFromPercent(const double& mealCost, const int& percent) c
     return (mealCost * percent) / 100;
 }
 
+double Solution::totalCost(const double& mealCost, const int& tipPercent, const int& taxPercent) {
+    double tip = amountFromPercent(mealCost, tipPercent);
+    double tax = amountFromPercent(mealCost, taxPercent);
+
+    return mealCost + tip + tax;
+}
+
+std::string Solution::formattedResult(const double& mealCost, const int& tipPercent, const int& taxPercent) {
+    double roundedValue = round(Solution::totalCost(mealCost, tipPercent, taxPercent));
+    std::stringstream stream;
+    stream << "The total meal cost is " << roundedValue << " dollars." << std::endl; 
+    return stream.str();
+}
 void Solution::printResult(const double& mealCost, const int& tipPercent, const int& taxPercent) {
-    (void)mealCost;
-    (void)tipPercent;
-    (void)taxPercent;
+    std::cout << Solution::formattedResult(mealCost, tipPercent, taxPercent);
 }
--- ThirtyDaysOfCode/Operators/solution.h
+++ ThirtyDaysOfCode/Operators/solution.h
@@ -3,11 +3,14 @@
 
 #include<string>
 #include<iostream>
+#include<sstream>
 #include<cmath>
 
 class Solution {
     public:
         double amountFromPercent(const double& mealCost, const int& percent) const;
+        double totalCost(const double& mealCost, const int& tipPercent, const int& taxPercent);
+        std::string formattedResult(const double& mealCost, const int& tipPercent, const int& taxPercent);
         void printResult(const double& mealCost, const int& tipPercent, const int& taxPercent);
 };
 
