c64908d0ffc0029decd1d19cf16a1862ad3ac3b7
Saydul Bashar <tamimnoone@gmail.com>
2017-07-26 20:22:11 +0000

    Removed redundant if statements.

--- ThirtyDaysOfCode/IntroToConditionalStatements/solution.cpp
+++ ThirtyDaysOfCode/IntroToConditionalStatements/solution.cpp
@@ -3,14 +3,10 @@
 std::string Solution::calculate(const int& inputNumber) {
     if(inputNumber % 2 != 0) {
         return "Weird";
-    } else {
-        if( inputNumber >= 6 && inputNumber <= 20) {
+    } else if( inputNumber >= 6 && inputNumber <= 20) {
             return "Weird";
-        } else {
-            return "Not Weird";
-        }
     }
-    return "Weird";
+    return "Not Weird";
 }
 void Solution::printResult(const int& inputNumber) {
     std::cout << Solution::calculate(inputNumber) << std::endl;
