79046fa3f2da9927b636626eeb647a2a8fb6f55f
Saydul Bashar <sbashar@idirect.net>
2017-08-29 14:21:17 +0000

    Added a bit more code to the test class.

--- ThirtyDaysOfCode/Recursion/test.cpp
+++ ThirtyDaysOfCode/Recursion/test.cpp
@@ -13,3 +13,8 @@ class SolutionTest : public ::testing::Test {
 TEST_F(SolutionTest, test0Factorial) {
 
 }
+
+int main(int argc, char ** argv) {
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
